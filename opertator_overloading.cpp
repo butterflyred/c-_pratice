23540ad8f4d655541f15128e650fca89a74640ef
butterflyred <a11025112@gmail.com>
2021-01-15 13:23:55 +0800

    operator overloading2

--- opertator_overloading.cpp
+++ opertator_overloading.cpp
@@ -1,77 +1,28 @@
  #include <iostream>
-// https://www.programmersought.com/article/73522283407/
 using namespace std;
-class Point {
-private:
-    /* data */
-    int x,y;
-public:
-    Point(/* args */);
-    Point(/* args */int a ,int b);
-    Point(const Point& r);
-    bool operator==(const Point& r);
-    ~Point();
-};
 
-bool Point::operator==(const Point& r){
-    cout << "opertator_overloading" << endl; 
-    if (this == &r)
-    {
-        /* code */
-        return true;
-    }
-    if (this->x == r.x)
-    {
-        /* code */
-        return true;
-    }
-    return false;
-    
+template <typename T>
+void Swap(T& a,T& b){
+    T t =a;
+    a = b;
+    b=t;
 }
-
-Point::Point(/* args */) :x(0),y(0)
-{
-    cout << "create constructor" << endl;
-}
-Point::Point(/* args */int a ,int b) :x(a),y(b)
-{
-    cout << "create constructor" << endl;
-}
-
-Point::Point(/* args */const Point& r) :x(r.x),y(r.y)
-{
-    cout << "copy constructor" << endl;
-    x = r.x; // shallow copy
-    char* str; // 先宣告新的空間
-    str = new char[x]; // deep copy
-}
-
-Point::~Point()
-{
-    cout << "de constructor" << endl;
-}
-
-
 int main(int argc, char const *argv[])
 {
-    cout << "ptr 1 " << endl;
-    Point ptr1(11,1);
-    cout << "ptr 2 " << endl;
-    Point ptr2 = ptr1; // copy constructor
-    cout << "ptr 3 " << endl;
-    Point ptr3(ptr1); // copy constructor
+    int a = 10;
+    int b = 20;
+         Swap(a, b); //Automatically push to call
+         //Swap<int>(a, b);//Display the specified call
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+ 
+    float c = 12.3;
+    float d = 23.4;
+ 
+         //Swap(c, d); //Automatically push to call
+         Swap<float>(c, d); //Display the specified call
+    cout << "c = " << c << endl;
+    cout << "d = " << d << endl;
 
-    if(ptr1 == ptr2){
-
-        cout << "ptr1 == ptr2 " << endl;
-    }
-    if(ptr1 == ptr3){
-
-        cout << "ptr1 == ptr3 " << endl;
-    }
-    if(ptr2 == ptr3){
-
-        cout << "ptr2 == ptr3 " << endl;
-    }
     return 0;
 }
